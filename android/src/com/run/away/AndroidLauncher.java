79f2aa83c4ed6f0827bc8af7b69420440c93a30b
liam humphreys <liam.humphreys1989@gmail.com>
2019-05-09 08:58:36 +0100

    initial commit

---
+++ android/src/com/run/away/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.run.away;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.run.away.RunAway;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new RunAway(), config);
+	}
+}
---
+++ desktop/src/com/run/away/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.run.away.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.run.away.RunAway;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new RunAway(), config);
+	}
+}
